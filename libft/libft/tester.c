ae2498d67562854bbb0ab678a906b1a700a85ffc
Heartless <fabio.facilla@gmail.com>
2020-12-15 16:57:51 +0100

    added ft_bzero

--- libft/libft/tester.c
+++ libft/libft/tester.c
@@ -5,11 +5,116 @@
 #define VERSION_NUMBER 2.0
 #define TEST_MODULE "libft"
 
-void test_ft_strlen()
+void	test_ft_memset()
+{
+	char TEST_NAME[] = "ft_memset test 1.0";
+	char memset_test0[] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	char memset_test1[] = "XX\nXX\bXX\vX";
+	char memset_test2[] = "XXXXX\0XXXX";
+	char memset_control0[] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	char memset_control1[] = "XX\nXX\bXX\vX";
+	char memset_control2[] = "XXXXX\0XXXX";
+
+	char *ctrl0 = memset(memset_control0,89,30);//standard check, nothing special
+	//Memset throws a compiler warning on a empty string check so it's missing.
+	char *ctrl1 = memset(memset_control1,89,10);//special char check
+	char *ctrl2 = memset(memset_control2,89,10);//NULL char check
+	printf("HACS %.1f, %s, %s\n\n",VERSION_NUMBER,TEST_MODULE,TEST_NAME);
+	printf("EXPECTED RESULT (memset)\n");
+	printf("%s \n",ctrl0);
+	printf("%s \n",ctrl1);
+	printf("%s \n\n",ctrl2);
+	printf("ACTUAL RESULT   (ft_memset)\n");
+	char *ctrl3 = ft_memset(memset_test0,89,30);//standard check, nothing special
+	char *ctrl4 = ft_memset(memset_test1,89,10);//special char check
+	char *ctrl5 = ft_memset(memset_test2,89,10);//NULL char check
+	printf("%s \n",ctrl3);
+	printf("%s \n",ctrl4);
+	printf("%s \n",ctrl5);
+
+	if(strcmp(ctrl0,ctrl3) != 0)
+		printf("issue detected on test 0 (\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\",89,30): base test, nothing special\n");
+	if(strcmp(ctrl1,ctrl4) != 0)
+		printf("issue detected on test 1 (\"XX\\nXX\\bXX\\vX\",89,10): standard special chars test\n");
+	if(strcmp(ctrl2,ctrl5) != 0)
+		printf("issue detected on test 2 (\"XXXXX\\0XXXX\",89,10): check if there are issues on special characters\n");
+}
+void	test_ft_bzero()
+{
+	char TEST_NAME[] = "ft_bzero test 1.0";
+
+	char bzero_test0[] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXOK";//standard check, nothing special
+	char bzero_test1[] = "XX\nXX\bXX\vXOK";//special char check
+	char bzero_test2[] = "XXXXX\0XXXXOK";//NULL char check
+	char bzero_test3[] = "OK";//empty string check
+	char bzero_control0[] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXOK";//standard check, nothing special
+	char bzero_control1[] = "XX\nXX\bXX\vXOK";//special char check
+	char bzero_control2[] = "XXXXX\0XXXXOK";//NULL char check
+	char bzero_control3[] = "OK";//empty string check
+
+	int i = 0;
+	//using memset with 0 as second param since bzero is a legacy function and
+	//my compiler wont let me use it :(
+	memset(bzero_control0,0,30);//standard check, nothing special
+	memset(bzero_control1,0,10);//special char check
+	memset(bzero_control2,0,10);//terminating char check
+	memset(bzero_control3,0,0);//empty string check
+
+	ft_bzero(bzero_test0,30);//standard check, nothing special
+	ft_bzero(bzero_test1,10);//special char check
+	ft_bzero(bzero_test2,10);//terminating char check
+	ft_bzero(bzero_test3,0);//empty string check
+
+	printf("HACS %.1f, %s, %s\n\n",VERSION_NUMBER,TEST_MODULE,TEST_NAME);
+	printf("EXPECTED RESULT (bzero)\n");
+	i=0;
+	while(bzero_control0[i] == 0)
+		i++;
+	printf("%c%c\n",bzero_control0[i],bzero_control0[i+1]);
+	i=0;
+	while(bzero_control1[i] == 0)
+		i++;
+	printf("%c%c\n",bzero_control1[i],bzero_control1[i+1]);
+	i=0;
+	while(bzero_control2[i] == 0)
+		i++;
+	printf("%c%c\n",bzero_control2[i],bzero_control2[i+1]);
+	i=0;
+	while(bzero_control3[i] == 0)
+		i++;
+	printf("%c%c\n",bzero_control3[i],bzero_control3[i+1]);
+	printf("ACTUAL RESULT   (ft_bzero)\n");
+	i=0;
+	while(bzero_test0[i] == 0)
+		i++;
+	printf("%c%c\n",bzero_test0[i],bzero_test0[i+1]);
+	i=0;
+	while(bzero_test1[i] == 0)
+		i++;
+	printf("%c%c\n",bzero_test1[i],bzero_test1[i+1]);
+	i=0;
+	while(bzero_test2[i] == 0)
+		i++;
+	printf("%c%c\n",bzero_test2[i],bzero_test2[i+1]);
+	i=0;
+	while(bzero_test3[i] == 0)
+		i++;
+	printf("%c%c\n",bzero_test3[i],bzero_test3[i+1]);
+
+	if(strcmp(bzero_control0,bzero_test0) != 0)
+		printf("issue detected on test 0 (\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXOK\",30): standard check, nothing special\n");
+	if(strcmp(bzero_control1,bzero_test1) != 0)
+		printf("issue detected on test 1 (\"XX\\nXX\\bXX\\vXOK\",10): special char check\n");
+	if(strcmp(bzero_control2,bzero_test2) != 0)
+		printf("issue detected on test 2 (\"XXXXX\\0XXXXOK\",10): terminating char check\n");
+	if(strcmp(bzero_control3,bzero_test3) == 0)
+		printf("issue detected on test 2 (\"OK\",0): empty string check\n");
+}
+void	test_ft_strlen()
 {
 	char TEST_NAME[] = "ft_strlen test 1.0";
-	char strlen_test0[] = "13 chr string";//base test, nothing special
-	char strlen_test1[] = "This\tstring\vis\rfull\aof\bspecial\echaracters\n";//check if there are issues on special characters
+	char strlen_test0[] = "13 chr string";//standard check, nothing special
+	char strlen_test1[] = "This\tstring\vis\rfull\aof\bspecial\echaracters\n";//special char check
 	char strlen_test2[] = "";//empty string check
 
 	printf("HACS %.1f, %s, %s\n\n",VERSION_NUMBER,TEST_MODULE,TEST_NAME);
@@ -31,11 +136,11 @@ void test_ft_strlen()
 		printf("issue detected on test 2 (\"\"): empty string check\n");
 
 }
-void test_ft_strcpy()
+void	test_ft_strcpy()
 {
 	char TEST_NAME[] = "ft_strcpy test 1.0";
-	char strcpy_test0[] = "13 chr string";//base test, nothing special
-	char strcpy_test1[] = "This\tstring\vis\rfull of\bspecial\echaracters\n";//check if there are issues on special characters
+	char strcpy_test0[] = "13 chr string";//standard check, nothing special
+	char strcpy_test1[] = "This\tstring\vis\rfull of\bspecial\echaracters\n";//special char check
 	char strcpy_test2[] = "";//empty string check
 
 	char strcpy_dest0[60];
@@ -63,42 +168,8 @@ void test_ft_strcpy()
 	if(strcmp(strcpy_dest2,strcpy_dest5) != 0)
 		printf("issue detected on test 2 (\"\"): empty string check\n");
 }
-void test_ft_memset()
-{
-	char TEST_NAME[] = "ft_memset test 1.0";
-	char memset_test0[] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-	char memset_test1[] = "XX\nXX\bXX\vX";
-	char memset_test2[] = "XXXXX\0XXXX";
-	char memset_control0[] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-	char memset_control1[] = "XX\nXX\bXX\vX";
-	char memset_control2[] = "XXXXX\0XXXX";
 
-	char *ctrl0 = memset(memset_control0,89,30);//normal test, nothing special
-	//Memset throws a compiler warning on a empty string check so it's missing.
-	char *ctrl1 = memset(memset_control1,89,10);//empty string check
-	char *ctrl2 = memset(memset_control2,89,10);//terminating char check
-	printf("HACS %.1f, %s, %s\n\n",VERSION_NUMBER,TEST_MODULE,TEST_NAME);
-	printf("EXPECTED RESULT (memset)\n");
-	printf("%s \n",ctrl0);
-	printf("%s \n",ctrl1);
-	printf("%s \n\n",ctrl2);
-	printf("ACTUAL RESULT   (ft_memset)\n");
-	char *ctrl3 = ft_memset(memset_test0,89,30);//normal test, nothing special
-	char *ctrl4 = ft_memset(memset_test1,89,10);//empty string check
-	char *ctrl5 = ft_memset(memset_test2,89,10);//terminating char check
-	printf("%s \n",ctrl3);
-	printf("%s \n",ctrl4);
-	printf("%s \n",ctrl5);
-
-	if(strcmp(ctrl0,ctrl3) != 0)
-		printf("issue detected on test 0 (\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\"): base test, nothing special\n");
-	if(strcmp(ctrl1,ctrl4) != 0)
-		printf("issue detected on test 1 (\"XX\\nXX\\bXX\\vX\"): standard special chars test\n");
-	if(strcmp(ctrl2,ctrl5) != 0)
-		printf("issue detected on test 2 (\"XXXXX\\0XXXX\"): check if there are issues on special characters\n");
-}
-
-int main(int ac, char **av)
+int		main(int ac, char **av)
 {
 	if (ac == 2)
 	{
@@ -108,6 +179,8 @@ int main(int ac, char **av)
 			test_ft_strcpy();
 		else if (strcmp(av[1], "memset") == 0 || strcmp(av[1], "ft_memset") == 0)
 			test_ft_memset();
+		else if (strcmp(av[1], "bzero") == 0 || strcmp(av[1], "ft_bzero") == 0)
+			test_ft_bzero();
 	}
 	return (0);
 }
