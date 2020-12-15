822fee513b290ef59ecddd8d39671a397c6289fd
Heartless <fabio.facilla@gmail.com>
2020-12-15 20:11:14 +0100

    added ft_memmove

---
+++ libft/libft/ft_memmove.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+
+// Copies n characters from memory area source to
+// memory area dest
+
+// this will not generate issues if the memory blocks overlap.
+// unlike memcpy
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*destination;
+	const unsigned char	*source;
+
+	destination = (unsigned char*)dest;
+	source = (const unsigned char*)src;
+	if (destination < source)
+	{
+		while (n--)
+			*destination++ = *source++;
+	}
+	else
+	{
+		destination = (unsigned char*)dest + (n - 1);
+		source = (const unsigned char*)src + (n - 1);
+		while (n--)
+			*destination-- = *source--;
+	}
+	return (dest);
+}
--- libft/libft/libft.h
+++ libft/libft/libft.h
@@ -9,6 +9,7 @@ void	*ft_memset(void *ptr, int x, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlen(const char *str);
 char	*ft_strcpy(char* destination, const char* source);
 
