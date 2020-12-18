43ebcf0e1005afb90d2058609ea6b1445f85ddfb
Heartless <fabio.facilla@gmail.com>
2020-12-19 00:55:24 +0100

    cleaned the code a little

--- libft/libft/part 2/ft_memalloc.c
+++ libft/libft/part 2/ft_memalloc.c
@@ -11,9 +11,6 @@ void	*ft_memalloc(size_t amount)
 	address = (void *)malloc(amount);
 	if (address == NULL)
 		return (NULL);
-	else
-	{
-		ft_memset(address,0,amount);
-		return (address);
-	}
+	ft_memset(address,0,amount);
+	return (address);
 }
